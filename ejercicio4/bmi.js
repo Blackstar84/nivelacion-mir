48e1cb8f58f8ceff3d489125b34644b4c3fd6ee2
Blackstar84 <carlosalmada84@gmail.com>
2022-11-18 20:41:02 -0300

    se agrega el ejercicio 4 de JavaScript

---
+++ ejercicio4/bmi.js
@@ -0,0 +1,7 @@
+function BMI(peso, altura){
+    return peso / (altura**2)
+}
+
+console.log(BMI(65, 1.8))
+console.log(BMI(72, 1.6))
+console.log(BMI(52, 1.75))
---
+++ ejercicio4/suma.js
@@ -0,0 +1,9 @@
+function Suma(numero){
+    let suma=0;
+    for (let i = 1; i <= numero; i++) {
+        suma += i;
+    }
+    return suma;
+}
+
+console.log(Suma(4));
