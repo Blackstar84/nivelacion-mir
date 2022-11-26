a1d99149fad7a3d5e02ea2e51dfb1e7f889f7796
Blackstar84 <carlosalmada84@gmail.com>
2022-11-25 21:11:16 -0300

    se sube el bonus

--- ejercicio7/index.js
+++ ejercicio7/index.js
@@ -14,3 +14,34 @@ for (let i = 0; i < receta.ingredientes.length; i++) {
 }
 
 console.log(suma);
+
+let vecesRepetido=0;
+let letraRepetida='';
+
+function maxCaracter(palabra){
+    let palabra = palabra.toUpperCase() ;
+    for(let i = 0; i < palabra.length; i++){
+        let re = new RegExp("[^"+ palabra[i] +"]","g");
+        let repetido=palabra.replace(re, "");
+        
+        if(repetido.length >= vecesRepetido){
+            vecesRepetido=repetido.length;
+            letraRepetida=repetido[0];
+        }
+    }
+    return letraRepetida;
+}
+
+console.log(maxCaracter("abcccccd")) // "c"
+console.log(maxCaracter("manzana 12311111")) // "1"
+
+
+function palindrome(str) {
+    str = str.toLowerCase();
+    const strReversed = str.split("").reverse().join("");
+  
+    return strReversed === str ? "es palindromo" : "no es palindromo";
+  }
+  console.log(palindrome("Amor a Roma")) // Output: true
+
+console.log(palindrome("vamos makers!")) // Output: false
